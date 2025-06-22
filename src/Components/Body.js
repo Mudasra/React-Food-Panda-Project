33c1841314427f6ca06e80b47917caaa6d9fa585
Mudasra <km4446245@gmail.com>
2025-06-22 16:58:46 +0500

    Food Panda

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,47 +1,3 @@
-// by manually putting dummy data
-
-// import RestaurantCard from "./RestaurantCard";
-// import restaurantList from "../utils/MockData";
-// import SearchBar from "./SearchBar.js";
-// import Footer from "./Footer.js"
-// import { useState , useEffect } from "react";
-
-// const Body = () => {
-
-//   const [restaurants , setRestaurants] = useState(restaurantList);
-
-//   const filterTopRated = () => {
-//     const filtered = restaurantList.filter(res => parseFloat(res.rating) > 4);
-//     setRestaurants(filtered);
-//   };
-
-//   return (
-//     <div className="body">
-//       <div className="search">
-//         <SearchBar onFilter={filterTopRated}/>
-//       </div>
-
-//        <div className="res-container">
-//          {restaurants.map((res) => (
-//           <RestaurantCard
-//           key = {res.id}
-//           name = {res.name}
-//           image = {res.image}
-//           rating = {res.rating}
-//           cuisine = {res.cuisine}
-//           />
-//         ) )}
-//       </div>
-
-//       <Footer/>
-
-//     </div>
-//   )
-
-// }
-
-// export default Body;
-
 // by api call
 
 import RestaurantCard from "./RestaurantCard";
@@ -129,3 +85,60 @@ const Body = () => {
 };
 
 export default Body;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// by manually putting dummy data
+
+// import RestaurantCard from "./RestaurantCard";
+// import restaurantList from "../utils/MockData";
+// import SearchBar from "./SearchBar.js";
+// import Footer from "./Footer.js"
+// import { useState , useEffect } from "react";
+
+// const Body = () => {
+
+//   const [restaurants , setRestaurants] = useState(restaurantList);
+
+//   const filterTopRated = () => {
+//     const filtered = restaurantList.filter(res => parseFloat(res.rating) > 4);
+//     setRestaurants(filtered);
+//   };
+
+//   return (
+//     <div className="body">
+//       <div className="search">
+//         <SearchBar onFilter={filterTopRated}/>
+//       </div>
+
+//        <div className="res-container">
+//          {restaurants.map((res) => (
+//           <RestaurantCard
+//           key = {res.id}
+//           name = {res.name}
+//           image = {res.image}
+//           rating = {res.rating}
+//           cuisine = {res.cuisine}
+//           />
+//         ) )}
+//       </div>
+
+//       <Footer/>
+
+//     </div>
+//   )
+
+// }
+
+// export default Body;
+
