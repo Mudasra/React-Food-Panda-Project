d419ed2dd80da207358905647babf499bffd0b68
Mudasra <km4446245@gmail.com>
2025-06-22 18:33:27 +0500

    Food Panda

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,15 @@
-const Header = () =>{
+import { useState } from "react";
+
+const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container">
-        <img className="logo" src="https://www.foodpanda.com/wp-content/uploads/2023/06/foodpanda_logo_2023.svg"/>      
+        <img
+          className="logo"
+          src="https://www.foodpanda.com/wp-content/uploads/2023/06/foodpanda_logo_2023.svg"
+        />
       </div>
 
       <div className="nav-items">
@@ -10,13 +17,21 @@ const Header = () =>{
           <li>Home</li>
           <li>About Us</li>
           <li>Contact Us</li>
-          <li><i className="fa-solid fa-cart-shopping"></i></li>
+          <li>
+            <i className="fa-solid fa-cart-shopping"></i>
+          </li>
+          <button
+            className="login-btn"
+            onClick={() => {
+             btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
-  )
-}
-
-
+  );
+};
 
 export default Header;
